9dccbdbeea0b9f6f10cc860b980b644dbdb1aa60
Jeongbinpark <qkrwjdqls99@gmail.com>
2021-01-21 13:10:20 +0900

    get Video from youtube

--- popup.js
+++ popup.js
@@ -1,51 +1,60 @@
-const port = chrome.extension.connect({
-  name: "Vearch"
-});
-port.postMessage("Connected!");
-
-port.onMessage.addListener((message) => {
-  console.log(message);
-  if (message.msg === "video_source"){
-    console.log(message.data.src);
-    videoSrc_button_onClick(videoSrc_button.value, message.data.src);
-  }
-})
-
-
 window.onload = () =>{
   const input = document.getElementById("videoInput");
   const videoSrc_button = document.getElementById("videoSrc_button");
+  const search_button = document.getElementById("search_button");
+  
   videoSrc_button.addEventListener('click', ()=>{
     videoSrc_button_onClick(videoSrc_button.value, input.value);
    }, false);
+   search_button.addEventListener('click', ()=>{
+     search_button_onClick(search_button.value);
+    }, false);
   }
 
 
-const videoSrc_button_onClick = (state, text) => {
+const videoSrc_button_onClick = (state, videoSource) => {
   const input = document.getElementById("videoInput");
   const videoSrc_button = document.getElementById("videoSrc_button");
-  const src_text = document.getElementById("videoSrc");
+  const videoSrc = document.getElementById("videoSrc");
+  const videoSrcText = videoSrc.getElementsByTagName("h4")[0];
   const video = document.getElementById("video");
-  src_text.innerText = text;
+  
 
   if(state === "create"){
+    const reg = /[a-z0-9A-Z]*(?=\?|$)/;
+    const videoID = reg.exec(videoSource)[0];
+    let src = `https://youtube.com/${videoID}`;
+    let srcEmbed = `https://youtube.com/embed/${videoID}`;
+    videoSrcText.innerText = src;
+
+    //pass the src to backend to download
+
     input.style.display = "none";
     videoSrc_button.value = "delete";
     videoSrc_button.innerText = "delete";
-    src_text.style.display = "inline";
+    videoSrc.style.display = "inline";
 
-    video.setAttribute('src', text);
-    video.setAttribute('controls','controls');
+    video.setAttribute('src', srcEmbed);
 
   }
   else{
     input.style.display = "inline-block";
     videoSrc_button.value = "create";
     videoSrc_button.innerText = "create";
-    src_text.style.display = "none";
+    videoSrc.style.display = "none";
     input.value = "";
     video.removeAttribute('src');
-    video.removeAttribute('controls');
-    video.load();
+  }
+}
+
+const search_button_onClick = (keyword) => {
+  //check video
+  //if there's no video, float the message for empty source
+  //if not,
+  if(keyword === null){
+    //total script 
+  }
+  else {
+    //keyword script
   }
 }
