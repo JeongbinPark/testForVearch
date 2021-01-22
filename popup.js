80e9495865b3eed6c3cd1d597e0cfe10083ca0c8
Jeongbinpark <qkrwjdqls99@gmail.com>
2021-01-23 01:43:31 +0900

    get and display Video Script with fake data

--- popup.js
+++ popup.js
@@ -27,8 +27,28 @@ const videoSrc_button_onClick = (state, videoSource) => {
     let srcEmbed = `https://youtube.com/embed/${videoID}`;
     videoSrcText.innerText = src;
 
-    //pass the src to backend to download
+    //request and get scripts to server by passing video source or id
+    //for test(temporary)
+    pass_VideoSource(videoSource)
+      .then(()=> get_VideoScript())
+      .then((textScripts) => {
+        console.log(textScripts);
+        //make script list tags
+        makeScriptList(textScripts);  
+      })
+      .catch(alert);
 
+    //original 
+    /*    
+    getScriptData(videoSource)
+      .then((textScripts) => {
+        console.log(textScripts);
+        //make script list tags
+        makeScriptList(textScripts);  
+      })
+      .catch(alert); 
+    */
+      
     input.style.display = "none";
     videoSrc_button.value = "delete";
     videoSrc_button.innerText = "delete";
@@ -44,7 +64,70 @@ const videoSrc_button_onClick = (state, videoSource) => {
     videoSrc.style.display = "none";
     input.value = "";
     video.removeAttribute('src');
+    
+    let ul = document.getElementById("keywordLists");
+    while( ul.hasChildNodes() ){
+      ul.removeChild(ul.lastChild);
+    }
   }
+}
+
+
+//for test(temporary)
+const pass_VideoSource = (videoSource) =>{
+  return new Promise((resolve, reject)=>{
+    console.log(`pass video source to server : ${videoSource}`);
+    setTimeout(() => {
+      console.log("passed!");
+      resolve("passed!")
+    },1000);
+  })
+}
+//for test(temporary)
+const get_VideoScript = () => {
+  return new Promise((resolve, reject)=>{
+    console.log("get video script from server");
+    let scriptData = scripts;
+    setTimeout(() => {
+      if(scriptData !== null) resolve(scriptData);
+      else reject(new Error("Couldn't get video script!"));
+    }, 1000); 
+  })
+}
+
+//original 
+/* 
+//depending on the server, parameter can be video source or video id
+const getScriptData = async(videoSource) => {
+  let response = await fetch(`/scripts/${videoSource}`);  //request 
+  if (response.status === 200){
+    let scriptJSON = await response.json();
+    return scriptJSON;
+  }
+  else {
+    return new Error("Couldn't get video script!");
+  } 
+}
+*/ 
+
+
+const makeScriptList = (data) => {
+  if(data === null) return new Error("There is no script!");
+  let ul = document.getElementById("keywordLists");
+
+  data.forEach( el => {
+    let li = document.createElement("li");
+    let timestamp = document.createElement("div");
+    let keyword = document.createElement("div");
+    timestamp.appendChild(document.createTextNode(el.time));
+    timestamp.setAttribute("class", "timestamp");
+    keyword.appendChild(document.createTextNode(el.script));
+    keyword.setAttribute("class", "keyword");
+
+    li.appendChild(timestamp);
+    li.appendChild(keyword);
+    ul.appendChild(li);
+  });
 }
 
 const search_button_onClick = (keyword) => {
